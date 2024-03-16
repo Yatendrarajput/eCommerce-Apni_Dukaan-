f96449ac678b6449ee39c17a4ffb0cee4fab94ba
pranjali6 <itspranjali06@gmail.com>
2024-03-16 17:29:40 +0530

    backend commit

---
+++ server/routes/auth.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import {registerController,loginController,testController,} from '../controllers/authcontroller.js'
+import { isAdmin, requireSignIn } from '../middleware/authMiddleware.js'
+//router object
+const router = express.Router()
+
+//routing
+//register || method POST
+router.post('/register', registerController)
+
+//LOGIN || POST
+router.post('/login',loginController)
+
+//test routes
+router.get('/test', requireSignIn,isAdmin,testController);
+//there are two middlewares here in the fist one token is being checked, then admin is being checked
+export default router
