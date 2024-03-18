7891a14c1e2544f9bba0205d63b8eddf868ed994
pranjali6 <itspranjali06@gmail.com>
2024-03-18 13:33:36 +0530

    connecting client and server

--- client/src/Components/Layout/layout.js
+++ client/src/Components/Layout/layout.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+
+import {Toaster} from 'react-hot-toast';
 const layout = ({children}) => {
   return (
     <div>
    <Header/>
-   <main style={{minHeight:"70vh"}}>{children}</main>
+   <main style={{minHeight:"70vh"}}>{children}
+    <Toaster/>
+   </main>
    <Footer/>
     </div>
   )
