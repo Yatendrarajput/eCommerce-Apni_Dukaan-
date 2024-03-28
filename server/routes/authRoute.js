196ad76e457a7cffa3926c4f7bea952c9e68b8a3
pranjali6 <itspranjali06@gmail.com>
2024-03-29 01:47:25 +0530

    forgot password feature added

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerController,loginController,testController } from '../controllers/authcontroller.js'; 
+import { registerController,loginController,testController, forgotPasswordController } from '../controllers/authcontroller.js'; 
 import { isAdmin, requireSignIn } from '../middleware/authMiddleware.js';
 //router object
 const router = express.Router()
@@ -11,8 +11,12 @@ router.post('/Register', registerController);
 //LOGIN || POST
 router.post('/Login',loginController)
 
+//forgot password || POST
+router.post('/forgot-password',forgotPasswordController)
+
 //test routes
 router.get('/Test', requireSignIn,isAdmin,testController);
+
 //there are two middlewares here in the fist one token is being checked, then admin is being checked
 export default router
 
