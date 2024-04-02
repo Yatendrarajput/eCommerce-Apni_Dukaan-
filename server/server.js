36f322f2e0ae1b6e515bf3b0637c1502cf9bb2a1
Saksham Jain <sakshamj106@gmail.com>
2024-04-03 00:30:29 +0530

    product API done

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoute from './routes/authRoute.js';
+import productRoutes from './routes/productRoutes.js';
 import cors from 'cors'
 
 //configure env
@@ -22,6 +23,7 @@ app.use(morgan('dev'));
 
 //routes
 app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/product', productRoutes);
 
 //rest api
 app.get('/', (req,res) => {
